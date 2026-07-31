76f7d4c65e24e09b9e72a5a249071b342e9c3645
minsxo <kimminseo4216@gmail.com>
2026-07-31 22:19:20 +0900

    chore: Spring Boot 프로젝트 기본 세팅

---
+++ src/main/java/com/signal/signalbe/SignalBeApplication.java
@@ -0,0 +1,13 @@
+package com.signal.signalbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignalBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/signal/signalbe/SignalBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.signal.signalbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
